165908da953d09f76b9dba8dfc29550fd7b65573
dubblin27 <dubblin27@gmail.com>
2020-02-14 22:28:06 +0530

    1/100 DSA

---
+++ basic.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll ;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+//   //int a,b;
+//   string x ;
+// //   while (cin >> x) { // with 1.in
+// // // code
+// //     cout<< "hello\n" ;
+// //   }
+//
+//   getline(cin,x);
+//   cout << x ;
+  int a = 123456789;
+  ll b = (ll)a*a;
+  cout << b << "\n";
+}
