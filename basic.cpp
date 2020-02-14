e9c39e08307bf51f7e681241de70d6dec61fcfe3
dubblin27 <dubblin27@gmail.com>
2020-02-14 22:59:11 +0530

    1/100 DSA

--- basic.cpp
+++ basic.cpp
@@ -14,7 +14,15 @@ int main(){
 //
 //   getline(cin,x);
 //   cout << x ;
-  int128_t a = 123456789;
-  ll b = (ll)a*a;
-  cout << b << "\n";
+  // int64_t a = 123456789;
+  // ll b = (ll)a*a;
+  // cout << b << "\n";
+  ll x ;
+  ll m,n ;
+  cin >>x>> m ;
+  
+  x = x%m;
+  cout << x ;
+  if (x < 0) x += m;
+  cout << x ;
 }
